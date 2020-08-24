1d4b4439bec5066c68a566228d90f963b61779ce
Mukesh Patel <patelmukesh325@gmail.com>
2020-08-24 13:54:32 +0530

    properties

---
+++ Proj34-RealTime_MiniProj_WithProperties/src/main/java/com/nt/vo/CustomerVO.java
@@ -0,0 +1,16 @@
+package com.nt.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class CustomerVO {
+	private String cname;
+	private  String cadd;
+	private  String pAmt;
+	private String time;
+	private String rate;
+}
