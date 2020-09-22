bfef40604e807deb7e3f5caf3389249e28d2e456
Mukesh Patel <patelmukesh325@gmail.com>
2020-09-22 23:21:49 +0530

    miniProj-BeanLifeCycle

---
+++ SpringProj51-RealTime_MiniProj_SpringBeanLifeCycle/src/main/java/com/nt/vo/CustomerVO.java
@@ -0,0 +1,12 @@
+package com.nt.vo;
+
+import lombok.Data;
+
+@Data
+public class CustomerVO {
+	private String cname;
+	private  String cadd;
+	private  String pAmt;
+	private String time;
+	private String rate;
+}
