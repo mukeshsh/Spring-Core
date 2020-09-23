ba7138197cf0bf9ef5cf32b130e18269e4a0a812
Mukesh Patel <patelmukesh325@gmail.com>
2020-09-23 22:44:57 +0530

    AwareInjection-Anno

---
+++ SpringProj55Anno-Traditional-DependencyLookup-AwareInjection/src/main/java/com/nt/beans/Engine.java
@@ -0,0 +1,19 @@
+package com.nt.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component("engg")
+public class Engine {
+	
+	public Engine() {
+		System.out.println("Engine:: 0-param constructor");
+	}
+	
+	public  void start() {
+		System.out.println("Engine.start()--->Engine started");
+	}
+	
+	public  void stop() {
+		System.out.println("Engine: stop()--->Engine stopped");
+	}
+}
