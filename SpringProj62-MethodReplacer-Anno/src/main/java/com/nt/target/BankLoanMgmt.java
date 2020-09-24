cb5b1a58aa7098e14e61a0b978fb1a6f25330376
Mukesh Patel <patelmukesh325@gmail.com>
2020-09-24 13:47:22 +0530

    MethodReplacer-Anno

--- SpringProj62-MethodReplacer-Anno/src/main/java/com/nt/target/BankLoanMgmt.java
+++ SpringProj62-MethodReplacer-Anno/src/main/java/com/nt/target/BankLoanMgmt.java
@@ -2,7 +2,7 @@ package com.nt.target;
 
 public class BankLoanMgmt {
 	//final not overriden
-  public final float calculateIntrAmt(float pAmt,float rate,float time) {
+  public  float calculateIntrAmt(float pAmt,float rate,float time) {
 	  System.out.println("BankLoanMgmt.calculateIntrAmt()::Compound Intrest Amount");
 	  return (float) (pAmt*(Math.pow(1+rate/100, time))-pAmt);
   }
